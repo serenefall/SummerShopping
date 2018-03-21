2fea91685465845b000699b6ee049b99a0a125a5
tianxintao <ttx12345677@gmail.com>
2018-03-20 20:14:12 -0700

    Add files via upload

--- src/ui/customer.java
+++ src/ui/customer.java
@@ -1,4 +1,22 @@
 package ui;
 
+import javax.swing.*;
+
 public class customer {
+    private JButton viewOrderButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton searchProductButton;
+    private JTextField textField4;
+    private JButton deleteOrderButton;
+    private JButton buyButton;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField8;
+    private JTextField textField7;
+    private JButton rateSellerButton;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JButton rateProductButton;
 }
--- src/ui/log_in.java
+++ src/ui/log_in.java
@@ -1,4 +1,17 @@
 package ui;
 
+import javax.swing.*;
+
 public class log_in {
+    public JPanel Login;
+    private JButton signUpButton;
+    private JButton logInButton;
+    private JLabel Username;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JRadioButton adminRadioButton;
+    private JRadioButton sellerRadioButton;
+    private JRadioButton buyerRadioButton;
+
+
 }
