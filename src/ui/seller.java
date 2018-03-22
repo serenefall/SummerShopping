0c17a52f98a0e698e516b11de1d9f6f585a29e23
tianxintao <ttx12345677@gmail.com>
2018-03-22 15:40:42 -0700

    Add files via upload

--- src/ui/seller.java
+++ src/ui/seller.java
@@ -1,4 +1,23 @@
 package ui;
 
+import javax.swing.*;
+
 public class seller {
+    private JButton showProductsButton;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JButton addProductButton;
+    private JButton deleteProductButton;
+    private JButton updatePriceButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JButton totalRevenueButton;
+    private JTextField textField12;
+    private JButton goodCustomerButton;
 }
