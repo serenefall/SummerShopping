7fee53172e9ce110add1ff3e2075b0a72cd90db9
ray <yinsharp25@gmail.com>
2018-03-22 15:24:31 -0700

    sql updated
    Users table created to store the password of users

--- summershopping.sql
+++ summershopping.sql
@@ -1,5 +1,32 @@
+drop table Customer;
+drop table VIP_1;
+drop table VIP_2;
+drop table Products;
+drop table Seller;
+drop table PutOrder;
+drop table Choose;
+drop table Rate;
+drop table Has;
+drop table Users;
+
+CREATE TABLE Users (
+	id INT,
+	password INT,
+	type VARCHAR(50)
+) 
+
+INSERT INTO Users (id, password, type) values (234, 123456, 'customer');
+
+INSERT INTO Users (id, password, type) values (111111, 123456, 'seller');
+
+INSERT INTO Users (id, password, type) values (331, 593843, 'customer');
+
+INSERT INTO Users (id, password, type) values (128452, 341832, 'seller');
+
+INSERT INTO Users (id, password, type) values (1, 123456, 'admin');
+
 CREATE TABLE Customer (
-	Customer_id VARCHAR(50),
+	Customer_id INT,
             Name CHAR(50),
 	Phone# VARCHAR(50),
 	Billing_address VARCHAR(50),
@@ -7,15 +34,15 @@ CREATE TABLE Customer (
 PRIMARY KEY (Customer_id)
 );
 
-INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values ('69-864-7808', 'Lucas Snyder', '(100) 4741902', '288 Westerfield Court', '288 Westerfield Court');
+INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values (234, 'Lucas Snyder', '(100) 4741902', '288 Westerfield Court', '288 Westerfield Court');
 
-INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values ('09-254-7178', 'Cindy Newton', '(286) 5727517', '379 Parkside Circle', '93 Sachs Street');
+INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values (218, 'Cindy Newton', '(286) 5727517', '379 Parkside Circle', '93 Sachs Street');
 
-INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values ('89-747-8984', 'Delbert Park', '(437) 5130996', '99431 Almo Trail', '99431 Almo Trail');
+INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values (894, 'Delbert Park', '(437) 5130996', '99431 Almo Trail', '99431 Almo Trail');
 
-INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values ('97-952-3231', 'Becky Maxwell', '(573) 8030653', '53 Portage Crossing', '53 Portage Crossing');
+INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values (331, 'Becky Maxwell', '(573) 8030653', '53 Portage Crossing', '53 Portage Crossing');
 
-INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values ('87-914-8118', 'Brian Arnold', '(195) 1966652', '76891 Ludington Street', '97 Transport Park');
+INSERT INTO Customer (Customer_id, Name, Phone#, Billing_address, Shipping_address) values (879, 'Brian Arnold', '(195) 1966652', '76891 Ludington Street', '97 Transport Park');
 
 CREATE TABLE VIP_1 (
 	VIP_ID INT,
@@ -34,11 +61,11 @@ CREATE TABLE VIP_2 (
 	Annual_fee INT,
           PRIMARY KEY (Customer_id)
 );
-INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values ('02-349-8190', 1721, 20);
-INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values ('80-666-6892', 2997, 20);
-INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values ('91-091-5395', 2148, 20);
-INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values ('62-814-0066', 6724, 20);
-INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values ('56-426-5754', 3817, 20);
+INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values (234, 1721, 20);
+INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values (516, 2997, 20);
+INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values (948, 2148, 20);
+INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values (246, 6724, 20);
+INSERT INTO VIP_2 (Customer_id, VIP_ID, Annual_fee) values (384, 3817, 20);
 
 
 
@@ -97,15 +124,15 @@ CREATE TABLE PutOrder (
             FOREIGN KEY(Customer_id) REFERENCES Customers (Customer_id),
 			FOREIGN KEY(Seller_ID) REFERENCES Seller (Seller_ID)
 );
-INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (In_progress, 'maestro', '2/14/2017', NULL, NULL, NULL, 84601784, 45167, '07-129-9603',458715);
+INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (In_progress, 'maestro', '2017/2/14', '2017/2/15', '2017/2/19', NULL, 84601784, 45167, 234, 458715);
 
-INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (In_progress, 'mastercard', '2/15/2017', '2/18/2017', '2/21/2017', 23, 21784284, 97673, '73-247-6510',300356);
+INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (In_progress, 'mastercard', '2017/3/11', '2017/3/12', '2017/3/16', 23, 21784284, 97673, 510, 300356);
 
-INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (Completed, 'china-unionpay', '2/14/2017', '2/17/2017', '2/22/2017', 18, 93777720, 29453, '29-564-1413',897458);
+INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (Completed, 'china-unionpay', '2017/4/05', '2017/4/06', '2017/4/10', 18, 93777720, 29453, 413, 897458);
 
-INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (Completed, 'switch', '2/15/2017', '2/18/2017', '2/20/2017', 6, 62089060, 13511, '11-167-0896',128754);
+INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (Completed, 'switch', '2017/5/13', '2017/5/14', '2017/5/18', 6, 62089060, 13511, 896, 128754);
 
-INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (Completed, '2/14/2017', '2/19/2017', '2/20/2017', 8, 62735978, 10135, '82-469-4274',789458);
+INSERT INTO PutOrder (Status, Payment_method, Data_placed, Shipping_date, Arrival_date, VIP_points_used, Order_number , Product_ID, Customer_id, Seller_ID) values (Completed, 'paypal', '2017/2/11', '2017/2/12', '2017/2/16', 8, 62735978, 10135, 274, 789458);
 
 CREATE TABLE Choose (
 	Product_ID INT,
@@ -116,15 +143,15 @@ FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id),
 FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
 	);
 
-INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (94895, 87-914-8118, 10);
+INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (94895, 118, 10);
 
-INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (67889, 97-952-3231, 8);
+INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (67889, 234, 8);
 
-INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (94895, 89-747-8984, 16);
+INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (94895, 824, 16);
 
-INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (49510, 06-387-2139, 7);
+INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (49510, 139, 7);
 
-INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (37086, 03-155-8425, 4);
+INSERT INTO Choose (Product_ID, Customer_id, Quantity) values (37086, 425, 4);
 
 CREATE TABLE Rate (
 	Rating INT,
@@ -138,15 +165,15 @@ CREATE TABLE Rate (
             FOREIGN KEY(Seller_ID) REFERENCES Seller(Seller_ID),
 			FOREIGN KEY(Order_number) REFERENCES PutOrder(Order_number),
 );
-INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (9, '85-816-1228', 817540, 84895,62089060);
+INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (9, 234, 817540, 84895,62089060);
 
-INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (7, '70-219-1393', 280040, 30304,62012360);
+INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (7, 393, 280040, 30304,62012360);
 
-INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (7, '86-778-1860', 792888, 30874,64789060);
+INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (7, 860, 792888, 30874,64789060);
 
-INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (8, '85-905-9179', 711351, 72571,62088100);
+INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (8, 879, 711351, 72571,62088100);
 
-INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (8, '35-719-2187', 235278, 80786,61384060);
+INSERT INTO Rate (Rating, Customer_id, Seller_ID, Product_ID, Order_number) values (8, 187, 235278, 80786,61384060);
 
 CREATE TABLE Has (
 	Product_ID INT,
@@ -166,5 +193,11 @@ INSERT INTO Has (Product_ID, Seller_ID, Quantity, Price) values (62662, 989615, 
 INSERT INTO Has (Product_ID, Seller_ID, Quantity, Price) values (13776, 448548, 126, '$112.81');
 
 INSERT INTO Has (Product_ID, Seller_ID, Quantity, Price) values (86410, 601743, 156, '$169.82');
+
+commit;
+
+
+
+commit;
 
 
