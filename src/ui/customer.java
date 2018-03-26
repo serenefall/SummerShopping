fd3ccea70f5255f568ea2134a432f98e37caa989
tianxintao <ttx12345677@gmail.com>
2018-03-26 15:12:04 -0700

    customer

--- src/ui/customer.java
+++ src/ui/customer.java
@@ -36,6 +36,14 @@ public class customer {
     private JTextField textField10;
     private JButton rateProductButton;
     public JPanel CustomerUI;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JTextArea textArea4;
+    private JTextArea textArea5;
+    private JTextArea textArea6;
+    private JTextArea textArea7;
+    private JTextArea textArea8;
     private String customerID;
 
     public void setCustomerID(String customerID) {
